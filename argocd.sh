4ccb6f22e17aede30abc54a95de6c322b10d1473
vinay <vinaybkrishna10@gmail.com>
2025-07-30 16:16:37 +0530

    new testing

--- argocd.sh
+++ argocd.sh
@@ -8,3 +8,7 @@ imageTag=$3
 
 argocd app create ${component_name} --upsert --repo https://github.com/testVinaycicd/roboshop-helm.git --path . --dest-namespace default --dest-server https://kubernetes.default.svc --values env-${env}/${component_name}.yaml --helm-set imageTag=${imageTag}
 argocd app sync ${component_name}
+
+
+
+#argocd login a5d5d3fd81f7a460b806e2337d5aa0c6-1632824844.us-east-1.elb.amazonaws.com --username admin --password jfryO2UWtkPeY8di

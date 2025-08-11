eb1af9c5a4fdfd5c86603d72d21706fe839a5adb
vinay <vinaybkrishna10@gmail.com>
2025-08-11 20:10:29 +0530

    new testing

--- argocd.sh
+++ argocd.sh
@@ -1,10 +1,10 @@
-URL=$(kubectl get svc argocd-server -n argocd | grep argocd| awk '{print $4}')
-PASSWORD=$(kubectl get secrets -n argocd argocd-initial-admin-secret -o json | jq .data.password|xargs | base64 --decode)
-argocd login $URL --grpc-web --insecure --username admin --password $PASSWORD
-
 component_name=$1
 env=$2
 imageTag=$3
+
+PASSWORD=$(kubectl get secrets -n argocd argocd-initial-admin-secret -o json | jq .data.password|xargs | base64 --decode)
+argocd login argocd-${env}.mikeydevops1.online --grpc-web --insecure --username admin --password $PASSWORD
+
 
 argocd app create ${component_name} --upsert --repo https://github.com/testVinaycicd/roboshop-helm.git --path . --dest-namespace default --dest-server https://kubernetes.default.svc --values env-${env}/${component_name}.yaml --helm-set imageTag=${imageTag}
 argocd app sync ${component_name}
